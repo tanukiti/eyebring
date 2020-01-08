2f6114855ec793ba8c624bec8d57b3794ac85290
tanukiti <33892948+tanukiti@users.noreply.github.com>
2020-01-08 13:52:38 +0900

    練習

---
+++ js/sample.js
@@ -0,0 +1,21 @@
+$(function () {
+  var _window = $(window),
+    _header = $(".site__header"),
+    heroBottom;
+
+  _window.on("scroll", function () {
+    heroBottom = $(".main__img").height();
+    if (_window.scrollTop()) {
+      _header.addClass("transform");
+    } else {
+      _header.removeClass("transform");
+    }
+  });
+
+  _window.trigger("scroll");
+});
+
+$(function () {
+  var height = $("header").height();
+  $(".main__img").css("padding-top", height + 20);
+});
